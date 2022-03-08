67ed90588f10bc423115a2dbd3722ebc32f10f20
Peter Pullen <plpullen@gmail.com>
2022-03-07 20:06:16 -0500

    add second/minute selectability and brainstorm restructing notes

--- app.js
+++ app.js
@@ -9,13 +9,16 @@
 //-make timer display the number of times in a loop
 //-make program to stop automatically with completion of certain number of loops 
 //-make reset button to return outputs to '0'
-//use previous timer to make rest timer that runs between each interval timer
-//enable selection of seconds or minutes
+//-use previous timer to make rest timer that runs between each interval timer
+//-enable selection of seconds or minutes
+//enable interval and rest lengths to be mixed sec/min
+//enable timer to visibly display passing seconds even when units are minutes
 //make it look pretty
 
 let startButton = document.getElementById("start")
 let stopButton = document.getElementById("stop")
 let resetButton = document.getElementById("reset")
+let speed
 
 //regular interval variables
 let intTimer = document.getElementById("timer")
@@ -28,39 +31,28 @@ let audioTwo = new Audio('long_pop.mp3')
 let intQty
 let intQtyCount = 0
 let totalIntLength
+let intUnit
 
 //rest interval variables
 let restTimer = document.getElementById("restTimer")
 let restCount = 0
 let restLength
 let restStartVar
+let restUnit
 
-
-function restCounter(){
-    restLength = document.getElementById("restLength").value
-        if (restCount < restLength -1){
-        restCount++;
-        restTimer.innerText = restCount;
-        audioOne.play();
-        } else {
-        audioTwo.play();
-        restCount = 0
-        restTimer.innerText = restCount;
-       }
-    } 
 
 function myCounter(){
     intQty = document.getElementById("qty").value
-    intLength = Number(document.getElementById("length").value)
+    intLength = Number(document.getElementById("length").value) //use ternary operator to determine impact of units here? (sec?value:value x 60)
     restIntLength = Number(document.getElementById("restLength").value)
     totalIntLength = intLength + restIntLength
-
+//should I pull out the timing functionality for what is displayed and have it run asynchonously from what is tracking intervals?
     if (intQtyCount < intQty){
 
         if (timeCount < intLength -1){
         timeCount++;
         intTimer.innerText = timeCount;
-        restTimer.innerText = restCount; //this resets rest timer a this moment to display zero in a correct visual way
+        restTimer.innerText = restCount; //this resets rest timer at this moment to display zero in a correct visual way
         audioOne.play();
         } else if (timeCount == intLength -1){
         audioTwo.play();
@@ -82,9 +74,9 @@ function myCounter(){
             console.log("error")
         }
 
+        //can I force the loop to wait till the restCounter is finished before looping again with 
+        //Promises? Forced Synchronicity? 
 
-        //how do I force the loop to wait till the restCounter is finished before looping again? 
-        //Promises? Force Synchronicity? I think i can do it with simple math too.
     } else {
         stopTimer()
         timeCount = 0
@@ -93,7 +85,13 @@ function myCounter(){
 }
 
 function startTimer(){
-    startVar = setInterval(myCounter, 1000)
+    intUnit = document.getElementById("intUnit").value
+    // restUnit = document.getElementById("restUnit").value
+    if(intUnit == "min"){ //this produces an error after running the first loop
+        speed = 60000
+    }else {speed = 1000}
+console.log(speed)
+    startVar = setInterval(myCounter, speed)
 }
 
 function stopTimer(){
